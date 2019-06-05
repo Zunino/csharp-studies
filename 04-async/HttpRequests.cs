1511ac7e9edd590c50f4f1342e20711a481e985c
Ney André de Mello Zunino <ney.zunino@softplan.com.br>
2019-06-05 16:11:53 -0300

    Separated the HTTP request code from the driver program.

---
+++ 04-async/HttpRequests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace DotnetStudies
+{
+
+    class HttpRequests
+    {
+        const string BEECEPTOR_URL_1 = "https://zunino.free.beeceptor.com/foo";
+        const string BEECEPTOR_URL_2 = "https://zunino.free.beeceptor.com/bar";
+        const string BEECEPTOR_URL_3 = "https://zunino.free.beeceptor.com/baz";
+
+        const int NO_OF_REQUESTS = 3;
+
+        static WebResponse GetUrl(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            Console.Write("GETting {0}...", url);
+            HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+            Console.WriteLine(" {0}", response.StatusCode);
+            return response;
+        }
+
+        static async Task<WebResponse> GetUrlAsync(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            Console.WriteLine("GETting {0}...", url);
+            return await request.GetResponseAsync();
+        }
+
+        static string GetBody(WebResponse response)
+        {
+            using (Stream stream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream);
+                string body = reader.ReadToEnd();
+                return body;
+            }
+        }
+
+        internal static void MakeSynchronousRequests()
+        {
+            Console.WriteLine("==> Request 1");
+            string body1 = GetBody(GetUrl(BEECEPTOR_URL_1));
+            Console.WriteLine("Response 1: {0}", body1);
+
+            Console.WriteLine("==> Request 2");
+            string body2 = GetBody(GetUrl(BEECEPTOR_URL_2));
+            Console.WriteLine("Response 2: {0}", body2);
+
+            Console.WriteLine("==> Request 3");
+            string body3 = GetBody(GetUrl(BEECEPTOR_URL_3));
+            Console.WriteLine("Response 3: {0}", body3);
+        }
+
+        internal static async void MakeAsynchronousRequests()
+        {
+            var tasks = new List<Task<WebResponse>>
+            {
+                GetUrlAsync(BEECEPTOR_URL_1),
+                GetUrlAsync(BEECEPTOR_URL_2),
+                GetUrlAsync(BEECEPTOR_URL_3)
+            };
+            for (int i = 0; i < NO_OF_REQUESTS; ++i)
+            {
+                int taskIdx = Task.WaitAny(tasks.ToArray());
+                var task = tasks[taskIdx];
+                tasks.RemoveAt(taskIdx);
+                try
+                {
+                    WebResponse response = await task;
+                    string body = GetBody(response);
+                    Console.WriteLine("Asynchronous response: {0}", body);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("=== [Exception] ===\n{0}", e.ToString());
+                    continue;
+                }
+            }
+            Console.WriteLine("All tasks processed");
+        }
+
+    }
+
+}
