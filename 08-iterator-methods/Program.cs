04aa4de6c1ee58492749c1c0e066858349fa3d66
Ney André de Mello Zunino <ney.zunino@softplan.com.br>
2019-07-09 15:28:36 -0300

    Example with a simple iterator method.

---
+++ 08-iterator-methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08_iterator_methods
+{
+    class Program
+    {
+        static IEnumerable<string> Colors()
+        {
+            yield return "RED";
+            yield return "GREEN";
+            yield return "BLUE";
+        }
+        static void Main(string[] args)
+        {
+            foreach (var color in Colors())
+            {
+                Console.WriteLine(color);
+            }
+        }
+    }
+}
