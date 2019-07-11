a2a6c0ffb4a955bcdc6562ffa5fbb61151a1b85d
Ney André de Mello Zunino <ney.zunino@softplan.com.br>
2019-07-11 15:42:28 -0300

    Added a publisher and some more games; separated insertion from query; games are no longer listed automatically for each publisher.

--- 09-ef-core/Program.cs
+++ 09-ef-core/Program.cs
@@ -1,6 +1,9 @@
 ﻿/**
  * Based on https://docs.microsoft.com/en-us/ef/core/get-started/netcore/new-db-sqlite
  *
+ * dotnet ef database drop
+ * dotnet ef database update
+ *
  * Andre Zunino <neyzunino@gmail.com>
  * 10 July 2019
  */
@@ -11,13 +14,28 @@ namespace _09_ef_core
 {
     class Program
     {
-        static void Main(string[] args)
+        static void InsertData()
         {
             using (var db = new GamesDbContext())
             {
                 var rockstar = new Publisher {
                     Name = "Rockstar Games",
                     Country = "USA"
+                };
+
+                var twok = new Publisher {
+                    Name = "2K Games",
+                    Country = "USA"
+                };
+
+                var bully = new Game {
+                    Title = "Bully",
+                    Year = 2005
+                };
+
+                var rdr = new Game {
+                    Title = "Red Dead Redemption",
+                    Year = 2010
                 };
 
                 var gtav = new Game {
@@ -30,13 +48,42 @@ namespace _09_ef_core
                     Year = 2018
                 };
 
+                var bioshock = new Game {
+                    Title = "Bioshock",
+                    Year = 2007
+                };
+
+                var bioshock2 = new Game {
+                    Title = "Bioshock 2",
+                    Year = 2010
+                };
+
+                var bioshockInfinite = new Game {
+                    Title = "Bioshock Infinite",
+                    Year = 2013
+                };
+
+                rockstar.Games.Add(bully);
+                rockstar.Games.Add(rdr);
                 rockstar.Games.Add(gtav);
                 rockstar.Games.Add(rdr2);
 
+                twok.Games.Add(bioshock);
+                twok.Games.Add(bioshock2);
+                twok.Games.Add(bioshockInfinite);
+
                 db.Publishers.Add(rockstar);
+                db.Publishers.Add(twok);
+
                 var count = db.SaveChanges();
                 Console.WriteLine("{0} records(s) saved to the database", count);
+            }
+        }
 
+        static void Main(string[] args)
+        {
+            using (var db = new GamesDbContext())
+            {
                 foreach (var publisher in db.Publishers)
                 {
                     Console.WriteLine("Publisher [ID: {0}]: {1} ({2})", publisher.PublisherId, publisher.Name, publisher.Country);
