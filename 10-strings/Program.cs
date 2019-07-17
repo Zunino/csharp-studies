c55af67ed84b0c99a50fb3789303d4ed2018d98c
Ney André de Mello Zunino <ney.zunino@softplan.com.br>
2019-07-17 09:57:15 -0300

    Example with verbatim strings, interpolated strings and a combination of the two.

---
+++ 10-strings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _10_strings
+{
+    class Program
+    {
+        private static string verbatim = @"I am a verbatim string.
+
+            My white space characters and line breaks are preserved.
+        ";
+
+        private static string InterpolatedString(string name, string email)
+        {
+            return $"{name}'s email is {email}";
+        }
+
+        private static string InterpolatedVerbatimString(string name, long amount)
+        {
+            return $@"Dear {name},
+
+Your payment of US{amount / 100:C} has been transferred
+to your checking account.
+
+Regards.
+";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("VERBATIM:\n{0}", verbatim);
+            Console.WriteLine("INTERPOLATED:\n{0}", InterpolatedString("Zunino", "neyzunino@gmail.com"));
+            Console.WriteLine("\nBOTH:\n{0}", InterpolatedVerbatimString("Zunino", 1550000));
+        }
+    }
+}
